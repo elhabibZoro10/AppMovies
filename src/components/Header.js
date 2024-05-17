10cb73fde4af16e33a3958e25ca11a93f8118b92
oualid <34518781+ouarrho@users.noreply.github.com>
2024-05-17 14:06:44 +0100

    css Page formatting

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div className="header">
+            <div className="container">
+                <div className="logo">
+                    <a href="#">Movies</a>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Header;
